f2a52a561276416e4eb12448886dd2a38123d55f
Sean Birdsall <sbirdsall0312@gmail.com>
2016-09-27 18:31:24 +0000

    Updating victims-volunteers exercise

---
+++ exercise/week2/main.js
@@ -0,0 +1,5 @@
+// Higher Order Functions - functions that take functions as arguments
+// Pure Functions - eliminate side effects, adding to but not changing what is already there
+//                - return new values that can be used or not
+//                - Immutability - data does NOT change, instead we create NEW data
+//                - Composability - modularize the code, bits & pieces
---
+++ exercise/week2/victims-volunteers/main.js
@@ -0,0 +1,61 @@
+var numOfVictims = 0;
+var numOfVols = 0;
+var victims = [];
+var moreEntries;
+var response;
+var vols = [];
+
+// Function to ask for initial or additional victims. Adds to total number of victims if reponded with the affirmative
+function askForVics() {
+    response = prompt("Do you have a victim to enter? (y or n)");
+    if (response.toLowerCase() === 'y') {
+    moreEntries  = true;
+    numOfVictims++;
+    } else {
+        moreEntries = false;
+    }
+}
+
+// Function to ask for initial or additional volunteers. Adds to total number of volunteers if reponded with the affirmative
+function askForVols() {
+    response = prompt("Do you have a volunteer to enter? (y or n)");
+    if (response.toLowerCase() === 'y') {
+    moreEntries  = true;
+    numOfVols++;
+    } else {
+        moreEntries = false;
+    }
+}
+// Ask
+askForVics();
+// Start loop as long as there are victim entries to be had
+while (moreEntries) {
+    // Adding a new object with 3 properties to the end of the victims array
+    victims.push({
+        name: prompt("Enter victim name here:"),
+        num: prompt("Enter victim phone number:"),
+        str: prompt("Enter victim street:")
+    });
+    askForVics();
+}
+
+askForVols();
+// Starting loop for volunteers
+while (moreEntries) {
+    // Adding a new object with 3 properties to the end of the vols array
+    vols.push({
+        name: prompt("Enter volunteer name here:"),
+        num: prompt("Enter volunteer phone number:"),
+        str: prompt("Enter volunteer street:")
+    });
+    askForVols();
+}
+// Window alert
+alert("The current disaster has " + numOfVictims + " victims." + "\n" +
+"We currently have " + numOfVols + " volunteers on hand." + "\n" +
+"Here is a list of people in need: " + victims.map(function(e){
+    return e.name;
+}) + "\n" +
+"And a list of all volunteers: " + vols.map(function(e){ // the paramater of the callback function represents each object in the array
+    return e.name;
+}));
