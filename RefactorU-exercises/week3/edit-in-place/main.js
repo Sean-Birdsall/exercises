1126d660980003faaeaa7655fc60ebf826a781fe
Sean Birdsall <sbirdsall0312@gmail.com>
2016-10-06 04:52:50 +0000

    Updating edit-in-place exercise

--- RefactorU-exercises/week3/edit-in-place/main.js
+++ RefactorU-exercises/week3/edit-in-place/main.js
@@ -18,30 +18,57 @@ function appController() {
     // Function to hide text when being edited
     main.hideText = function($event) {
         
-        // Reference the element that was clicked
-        var clickedElement = $event.currentTarget.nodeName;
-        switch (clickedElement){
+        // Reference the element node that was clicked
+        var clickedElementNode = $event.currentTarget.nodeName;
+        
+        switch (clickedElementNode){
             case 'H3':
                 main.editingName = true;
-                console.log(document.getElementById('head3'));
+                // In order to reference the input to give it focus, I had to add a delay to let it be 
+                // written to the DOM....I guess???
+                main.delayNameGrab = function(){
+                    main.clickedInput = document.getElementsByName('name')[0];     
+                    main.clickedInput.focus();
+                }
+                var timeoutID = window.setTimeout(main.delayNameGrab, 0);
+                
                 break;
             case 'SPAN':
                 // There is two span elements so we need to figure out which one is which
                 var clickedListItem = $event.currentTarget.getAttribute('id');
                 if (clickedListItem === 'age'){
                     main.editingAge = true;
+                    // Having to do a seperate function for each case annoys me
+                    main.delayAgeGrab = function(){
+                    main.clickedInput = document.getElementsByName('age')[0];     
+                    main.clickedInput.focus();
+                }
+                timeoutID = window.setTimeout(main.delayAgeGrab, 0);
                 } else if (clickedListItem === 'occ') {
                     main.editingOcc = true;
+                    // Having to do a seperate function for each case annoys me
+                    main.delayOccGrab = function(){
+                    main.clickedInput = document.getElementsByName('occ')[0];     
+                    main.clickedInput.focus();
+                }
+                timeoutID = window.setTimeout(main.delayOccGrab, 0);
                 }
                 break;
             case 'P':
                 main.editingBio = true;
+                // Having to do a seperate function for each case annoys me
+                    main.delayBioGrab = function(){
+                    main.clickedInput = document.getElementsByName('bio')[0];     
+                    main.clickedInput.focus();
+                }
+                timeoutID = window.setTimeout(main.delayBioGrab, 0);
         }
     }
     // Function to hide inputs and change the default text to the ng-model on inputs
     main.blurName = function($event) {
+        // Reference which element is being blurred by name of each element
         var blurredElement = $event.currentTarget.getAttribute('name');
-        console.log(blurredElement);
+        // Switch statement to take appropriate action depending on which element is being blurred
         switch (blurredElement){
             case 'name':
                 main.editingName = false;
